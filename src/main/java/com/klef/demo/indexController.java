d074356d8f5815f6882112a38f31a534ed97b738
Raga Hari Deep Molagazzela <harideep2002@gmail.com>
2021-10-29 15:06:51 +0530

    inital commit

---
+++ src/main/java/com/klef/demo/indexController.java
@@ -0,0 +1,14 @@
+package com.klef.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController 
+{
+	@RequestMapping("index")
+	public String index()
+	{
+		return "index.html";
+	}
+}
